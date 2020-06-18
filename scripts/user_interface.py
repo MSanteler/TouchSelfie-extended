886ef56cb87e5ed60679bab903cfd50915391dd9
Matthew Santeler <Matthew.Santeler@lendkey.com>
2020-06-17 20:46:38 -0400

    unsafe execture

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -163,6 +163,12 @@ class UserInterface():
                     callback()
             return safe_execute
 
+        # Factory to launch actions only when no snap is being processed
+        def unsafe_execute_factory(callback):
+            def safe_execute(args):
+                callback()
+            return unsafe_execute
+
         install_key_binding("snap_None",safe_execute_factory(lambda *args: self.snap("None")))
         install_key_binding("snap_Four",safe_execute_factory(lambda *args: self.snap("Four")))
         install_key_binding("snap_Animation",safe_execute_factory(lambda *args: self.snap("Animation")))
@@ -172,9 +178,9 @@ class UserInterface():
         install_key_binding("send_email",safe_execute_factory(lambda *args: self.send_email()))
         install_key_binding("configure",safe_execute_factory(lambda *args: self.long_press_cb(self)))
         install_key_binding("send_print",safe_execute_factory(lambda *args: self.send_print()))
-        install_key_binding("fx_forward",safe_execute_factory(lambda *args: self.__fx_forward()))
-        install_key_binding("fx_backward",safe_execute_factory(lambda *args: self.__fx_backward()))
-        install_key_binding("fx_clear",safe_execute_factory(lambda *args: self.__choose_specific_effect(1)))
+        install_key_binding("fx_forward",unsafe_execute_factory(lambda *args: self.__fx_forward()))
+        install_key_binding("fx_backward",unsafe_execute_factory(lambda *args: self.__fx_backward()))
+        install_key_binding("fx_clear",unsafe_execute_factory(lambda *args: self.__choose_specific_effect(1)))
         ## Bind keyboard keys to actions
 
         self.full_screen = config.full_screen
