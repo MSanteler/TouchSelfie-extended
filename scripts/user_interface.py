06d207a1572160ae4fb5f78c0ae6aad9982b3876
Matthew Santeler <Matthew.Santeler@lendkey.com>
2020-06-17 21:04:01 -0400

    Rollback

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -541,9 +541,6 @@ class UserInterface():
                 w_ = w * 2
                 h_ = h * 2
                 # take 4 photos and merge into one image.
-                self.log.debug("turning off suspend")
-                self.suspend_poll = False
-                self.log.debug("Suspend Poll: "+ str(self.suspend_poll))
                 self.__show_countdown(config.countdown1,annotate_size = 80)
                 self.camera.capture('collage_1.jpg')
                 self.__show_countdown(config.countdown2,annotate_size = 80)
@@ -552,9 +549,6 @@ class UserInterface():
                 self.camera.capture('collage_3.jpg')
                 self.__show_countdown(config.countdown2,annotate_size = 80)
                 self.camera.capture('collage_4.jpg')
-                self.log.debug("turning on suspend")
-                self.suspend_poll = True
-                self.log.debug("Suspend Poll: "+ str(self.suspend_poll))
                 # Assemble collage
                 self.camera.stop_preview()
                 self.status("Assembling collage")
